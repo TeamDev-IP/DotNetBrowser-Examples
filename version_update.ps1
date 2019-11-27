7fce190a9cba29116523fa5c4b44ad074250afa0
anna-dolbina <anna.dolbina@teamdev.com>
2019-11-27 12:11:08 +0200

    Add version update script.

---
+++ version_update.ps1
@@ -0,0 +1,9 @@
+param([String]$oldVersion = "1.21", [String]$newVersion = "1.21.1", [String]$oldVersionFull = "1.21.0.0", [String]$newVersionFull = "1.21.1.0")
+$oldVersionProjectName = "DotNetBrowser." + $oldVersion;
+$newVersionProjectName = "DotNetBrowser." + $newVersion;
+
+(Get-ChildItem -Include "*.csproj" -Recurse) | foreach {(Get-Content $_) | foreach { $_ -replace "Version=$($oldVersionFull)", "Version=$($newVersionFull)"} | Set-Content $_}
+
+(Get-ChildItem -Include "*.csproj" -Recurse) | foreach {(Get-Content $_) | foreach { $_ -replace $oldVersionProjectName, $newVersionProjectName} | Set-Content $_}
+
+(Get-ChildItem -Include "packages.config" -Recurse) | foreach {(Get-Content $_) | foreach { $_ -replace "version=`"$($oldVersion)", "version=`"$($newVersion)"} | Set-Content $_}
