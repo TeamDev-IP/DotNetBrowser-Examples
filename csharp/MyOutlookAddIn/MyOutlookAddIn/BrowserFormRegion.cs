9f7aef6f6a7dc39994c2b8b7d737b819ce647a8b
anna-dolbina <anna.dolbina@teamdev.com>
2019-12-03 17:02:28 +0200

    Add VSTO add-in sample.

---
+++ csharp/MyOutlookAddIn/MyOutlookAddIn/BrowserFormRegion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBrowser.Browser;
+using DotNetBrowser.Engine;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace MyOutlookAddIn
+{
+    partial class BrowserFormRegion
+    {
+        #region Form Region Factory 
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Note)]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("MyOutlookAddIn.BrowserFormRegion")]
+        public partial class BrowserFormRegionFactory
+        {
+            // Occurs before the form region is initialized.
+            // To prevent the form region from appearing, set e.Cancel to true.
+            // Use e.OutlookItem to get a reference to the current Outlook item.
+            private void BrowserFormRegionFactory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+                e.Cancel = false;
+            }
+        }
+
+        #endregion
+
+        private IEngine engine;
+        private IBrowser browser;
+
+        // Occurs before the form region is displayed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void BrowserFormRegion_FormRegionShowing(object sender, System.EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                engine = EngineFactory.Create(new EngineOptions.Builder()
+                {
+                    RenderingMode = RenderingMode.HardwareAccelerated
+                }.Build());
+                browser = engine.CreateBrowser();
+            })
+            .ContinueWith(t =>
+            {
+                browserView1.InitializeFrom(browser);
+                browser?.Navigation.LoadUrl("https://www.teamdev.com/dotnetbrowser");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+        }
+
+        // Occurs when the form region is closed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void BrowserFormRegion_FormRegionClosed(object sender, System.EventArgs e)
+        {
+            engine?.Dispose();
+        }
+    }
+}
---
+++ csharp/MyOutlookAddIn/MyOutlookAddIn/ThisAddIn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using DotNetBrowser.Logging;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace MyOutlookAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            LoggerProvider.Instance.Level = SourceLevels.Verbose;
+            LoggerProvider.Instance.FileLoggingEnabled = true;
+            LoggerProvider.Instance.OutputFile = "D:\\Projects\\MyOutlookAddIn\\DotNetBrowser.log";
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
