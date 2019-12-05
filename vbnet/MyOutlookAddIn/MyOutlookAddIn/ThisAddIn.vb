1315785e3a1728f9d0da96d53f3669d5589cddad
anna-dolbina <anna.dolbina@teamdev.com>
2019-12-05 17:49:13 +0200

    Add copyright and disclaimer. Update log file path.

--- vbnet/MyOutlookAddIn/MyOutlookAddIn/ThisAddIn.vb
+++ vbnet/MyOutlookAddIn/MyOutlookAddIn/ThisAddIn.vb
@@ -1,6 +1,29 @@
-﻿Imports System
+﻿#Region "Copyright"
+
+' Copyright 2019, TeamDev. All rights reserved.
+' 
+' Redistribution and use in source and/or binary forms, with or without
+' modification, must retain the above copyright notice and the following
+' disclaimer.
+' 
+' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+' "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+' LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+' A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+' OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+' LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+' DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+' THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#End Region
+
+Imports System
 Imports System.Collections.Generic
 Imports System.Diagnostics
+Imports System.IO
 Imports System.Linq
 Imports System.Text
 Imports System.Xml.Linq
@@ -13,7 +36,7 @@ Namespace MyOutlookAddIn
 		Private Sub ThisAddIn_Startup(ByVal sender As Object, ByVal e As System.EventArgs)
 			LoggerProvider.Instance.Level = SourceLevels.Verbose
 			LoggerProvider.Instance.FileLoggingEnabled = True
-			LoggerProvider.Instance.OutputFile = "D:\Projects\MyOutlookAddIn\DotNetBrowser.log"
+			LoggerProvider.Instance.OutputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"DotNetBrowser.log")
 		End Sub
 
 		Private Sub ThisAddIn_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs)
