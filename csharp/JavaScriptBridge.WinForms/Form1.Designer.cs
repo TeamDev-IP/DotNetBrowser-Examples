dfd88185e4b3057bbe3a57774d5755edfe78f2f6
anna-dolbina <anna.dolbina@teamdev.com>
2020-03-04 10:18:42 +0200

    Add example that demonstrates how to use JS-.NET bridge with WinForms.

---
+++ csharp/JavaScriptBridge.WinForms/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace JavaScriptBridge.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 404);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Sample text in Form";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(394, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update BrowserView >";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ csharp/JavaScriptBridge.WinForms/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using DotNetBrowser.Browser;
+using DotNetBrowser.Engine;
+using DotNetBrowser.Js;
+using DotNetBrowser.WinForms;
+
+namespace JavaScriptBridge.WinForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly IBrowser browser;
+        private readonly IEngine engine;
+
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+            BrowserView webView = new BrowserView {Dock = DockStyle.Fill};
+            tableLayoutPanel1.Controls.Add(webView, 1, 0);
+            tableLayoutPanel1.SetRowSpan(webView, 2);
+            engine = EngineFactory
+               .Create(new EngineOptions.Builder
+                           {
+                               RenderingMode = RenderingMode.HardwareAccelerated
+                           }
+                          .Build());
+            browser = engine.CreateBrowser();
+            webView.InitializeFrom(browser);
+            browser.ConsoleMessageReceived += (sender, args) => { };
+            browser
+               .MainFrame
+               .LoadHtml(@"<html>
+                        <head>
+                          <meta charset='UTF-8'>
+                          <style>body{padding: 0; margin: 0; width:100%; height: 100%;}
+                                textarea.fill {padding: 2; margin: 0; width:100%; height:100%;}
+                                button{position: absolute; bottom: 0; padding: 2; width:100%;}</style>
+                        </head>
+                        <body>
+                        <div>
+                        <textarea id='text' class='fill' autofocus cols='30' rows='20'>Sample text</textarea>
+                        <button id='updateForm' type='button' onClick='updateForm(document.getElementById(""text"").value)'>&lt; Update Form</button> 
+                        </div>
+                        </body>
+                        </html>")
+               .Wait();
+            IJsObject window = browser.MainFrame.ExecuteJavaScript<IJsObject>("window").Result;
+            window.Properties["updateForm"] = (Action<string>) UpdateForm;
+            FormClosing += Form1_FormClosing;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void UpdateForm(string value)
+        {
+            BeginInvoke((Action) (() => richTextBox1.Text = value));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IJsObject textElement = browser.MainFrame.ExecuteJavaScript<IJsObject>("document.getElementById('text');")
+                                           .Result;
+            textElement.Properties["value"] = richTextBox1.Text;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            engine?.Dispose();
+        }
+
+        #endregion
+    }
+}
