515d582b89a5cb629eeebf710bb5c5dd89e29cd3
anna-dolbina <anna.dolbina@teamdev.com>
2020-03-11 09:45:35 +0200

    Add VB.NET example.

---
+++ vbnet/FullScreen.WinForms/Form1.Designer.vb
@@ -0,0 +1,42 @@
+﻿Namespace FullScreen.WinForms
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.SuspendLayout()
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(800, 450)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+	End Class
+End Namespace
+
---
+++ vbnet/FullScreen.WinForms/Form1.vb
@@ -0,0 +1,89 @@
+﻿#Region "Copyright"
+
+' Copyright 2020, TeamDev. All rights reserved.
+' 
+' Redistribution and use in source and/or binary forms, with or without
+' modification, must retain the above copyright notice and the following
+' disclaimer.
+' 
+' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+' "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+' LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+' A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+' OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+' LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+' DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+' THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#End Region
+
+Imports DotNetBrowser.Browser
+Imports DotNetBrowser.Browser.Events
+Imports DotNetBrowser.Engine
+Imports DotNetBrowser.WinForms
+
+Namespace FullScreen.WinForms
+    ''' <summary>
+    '''     The example demonstrates how to implement custom full-screen handling.
+    ''' </summary>
+	Partial Public Class Form1
+		Inherits Form
+
+		Private ReadOnly browser As IBrowser
+		Private ReadOnly browserView As BrowserView
+		Private ReadOnly engine As IEngine
+		Private fullScreenForm As Form
+
+		#Region "Constructors"
+
+		Public Sub New()
+			InitializeComponent()
+			browserView = New BrowserView With {.Dock = DockStyle.Fill}
+			engine = EngineFactory.Create(New EngineOptions.Builder With {.RenderingMode = RenderingMode.HardwareAccelerated} .Build())
+			browser = engine.CreateBrowser()
+			AddHandler browser.FullScreenEntered, AddressOf OnFullScreenEntered
+			AddHandler browser.FullScreenExited, AddressOf OnFullScreenExited
+			browserView.InitializeFrom(browser)
+			browser.Navigation.LoadUrl("http://www.w3.org/2010/05/video/mediaevents.html")
+			Controls.Add(browserView)
+		End Sub
+
+		#End Region
+
+		#Region "Methods"
+
+		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+			browser?.Dispose()
+			engine?.Dispose()
+		End Sub
+
+		Private Sub OnFullScreenEntered(ByVal sender As Object, ByVal e As FullScreenEnteredEventArgs)
+			BeginInvoke(CType(Sub()
+										 fullScreenForm = New Form()
+										 fullScreenForm.TopMost = True
+										 fullScreenForm.ShowInTaskbar = False
+										 fullScreenForm.FormBorderStyle = FormBorderStyle.None
+										 fullScreenForm.WindowState = FormWindowState.Maximized
+										 fullScreenForm.Owner = Me
+
+										 Controls.Remove(browserView)
+										 fullScreenForm.Controls.Add(browserView)
+										 fullScreenForm.Show()
+			End Sub, Action))
+		End Sub
+
+		Private Sub OnFullScreenExited(ByVal sender As Object, ByVal e As FullScreenExitedEventArgs)
+			BeginInvoke(CType(Sub()
+										 fullScreenForm.Controls.Clear()
+										 Controls.Add(browserView)
+										 fullScreenForm.Hide()
+										 fullScreenForm.Close()
+			End Sub, Action))
+		End Sub
+
+		#End Region
+	End Class
+End Namespace
