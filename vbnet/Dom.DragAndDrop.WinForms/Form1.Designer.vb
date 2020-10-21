c94532816db3affc3d299780c792abe81ba879b6
anna-dolbina <anna.dolbina@gmail.com>
2020-10-21 14:27:21 +0300

    Add VB.NET example to demonstrate Drag and Drop interception for a DOM element.

---
+++ vbnet/Dom.DragAndDrop.WinForms/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿Namespace Dom.DragAndDrop.WinForms
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.browserView1 = New DotNetBrowser.WinForms.BrowserView()
+			Me.textBox1 = New System.Windows.Forms.TextBox()
+			Me.SuspendLayout()
+			' 
+			' browserView1
+			' 
+			Me.browserView1.Dock = System.Windows.Forms.DockStyle.Top
+			Me.browserView1.Location = New System.Drawing.Point(0, 0)
+			Me.browserView1.Name = "browserView1"
+			Me.browserView1.Size = New System.Drawing.Size(800, 328)
+			Me.browserView1.TabIndex = 0
+			' 
+			' textBox1
+			' 
+			Me.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+			Me.textBox1.Location = New System.Drawing.Point(0, 350)
+			Me.textBox1.Multiline = True
+			Me.textBox1.Name = "textBox1"
+			Me.textBox1.ReadOnly = True
+			Me.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+			Me.textBox1.Size = New System.Drawing.Size(800, 100)
+			Me.textBox1.TabIndex = 1
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(800, 450)
+			Me.Controls.Add(Me.textBox1)
+			Me.Controls.Add(Me.browserView1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		#End Region
+
+		Private browserView1 As DotNetBrowser.WinForms.BrowserView
+		Private textBox1 As System.Windows.Forms.TextBox
+	End Class
+End Namespace
+
