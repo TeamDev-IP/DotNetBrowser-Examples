8c12415b433aaf6f5a2424f468175f5f633814c3
anna-dolbina <anna.dolbina@gmail.com>
2020-10-21 14:20:55 +0300

    Add C# example to demonstrate Drag and Drop interception for a DOM element.

---
+++ csharp/Dom.DragAndDrop.WinForms/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Dom.DragAndDrop.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browserView1 = new DotNetBrowser.WinForms.BrowserView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // browserView1
+            // 
+            this.browserView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.browserView1.Location = new System.Drawing.Point(0, 0);
+            this.browserView1.Name = "browserView1";
+            this.browserView1.Size = new System.Drawing.Size(800, 328);
+            this.browserView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 350);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 100);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browserView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DotNetBrowser.WinForms.BrowserView browserView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
