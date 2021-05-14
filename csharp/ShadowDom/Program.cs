309143d8750499a3c4cc7e8e9c8f5e771b259570
anna-dolbina <anna.dolbina@gmail.com>
2021-05-14 14:00:10 +0300

    Add Shadow DOM example for C#

---
+++ csharp/ShadowDom/Program.cs
@@ -0,0 +1,97 @@
+﻿#region Copyright
+
+// Copyright 2021, TeamDev. All rights reserved.
+// 
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.IO;
+using DotNetBrowser.Browser;
+using DotNetBrowser.Dom;
+using DotNetBrowser.Engine;
+using DotNetBrowser.Geometry;
+using DotNetBrowser.Js;
+
+namespace ShadowDom
+{
+    /// <summary>
+    ///     This example demonstrates how to work with Shadow DOM.
+    /// </summary>
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                using (IEngine engine = EngineFactory.Create(new EngineOptions.Builder().Build()))
+                {
+                    Console.WriteLine("Engine created");
+
+                    using (IBrowser browser = engine.CreateBrowser())
+                    {
+                        browser.Size = new Size(1024, 768);
+                        Console.WriteLine("Browser created");
+                        browser.Navigation.LoadUrl(Path.GetFullPath("example.html")).Wait();
+
+                        Console.WriteLine("URL loaded");
+
+                        IDocument document = browser.MainFrame.Document;
+                        IJsObject container = document.GetElementById("container") as IJsObject;
+
+                        //Create shadow root.
+                        INode shadowRoot = container?.Invoke<INode>("attachShadow",
+                                                                   browser
+                                                                      .MainFrame
+                                                                      .ParseJsonString("{\"mode\": \"open\"}"));
+                        Console.WriteLine("Shadow root created: " + (shadowRoot != null));
+
+                        //Fetch shadow root.
+                        shadowRoot = container?.Properties["shadowRoot"] as INode;
+                        Console.WriteLine("Shadow root fetched: " + (shadowRoot != null));
+
+                        //Add node to shadow root.
+                        IElement inside = document.CreateElement("h1");
+                        inside.InnerText = "Inside Shadow DOM";
+                        inside.Attributes["id"] = "inside";
+                        shadowRoot?.Children.Append(inside);
+
+                        //Find new node in shadow root.
+                        IElement element = shadowRoot?.GetElementById("inside");
+                        Console.WriteLine("Inside element inner text: " + element?.InnerText);
+
+                        //Try finding the same node from the main document.
+                        element = document.GetElementById("inside");
+                        Console.WriteLine("Inside element found in the document: " + (element != null));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Press any key to terminate...");
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
