0ab12c69258e272c06e11511b2ec6c1587e8437d
anna-dolbina <anna.dolbina@teamdev.com>
2021-06-23 12:31:58 +0300

    Add Mvvm.Wpf example for C#

---
+++ csharp/Mvvm.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿#region Copyright
+
+// Copyright 2021, TeamDev. All rights reserved.
+// 
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.Windows;
+using DotNetBrowser.Engine;
+using Mvvm.Wpf.ViewModels;
+
+namespace Mvvm.Wpf
+{
+    /// <summary>
+    ///     This example demonstrates the possible approach to use DotNetBrowser
+    ///     with WPF data binding.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IEngine engine;
+
+        #region Properties
+
+        public MyBrowserViewModel MyBrowser { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            EngineOptions engineOptions = new EngineOptions.Builder
+                {
+                    RenderingMode = RenderingMode.HardwareAccelerated
+                }
+               .Build();
+            engine = EngineFactory.Create(engineOptions);
+
+            MyBrowser = new MyBrowserViewModel(engine.CreateBrowser())
+            {
+                Url = "www.teamdev.com/dotnetbrowser"
+            };
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void MainWindow_OnClosed(object sender, EventArgs e)
+        {
+            engine?.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ csharp/Mvvm.Wpf/Views/MyBrowserView.xaml.cs
@@ -0,0 +1,68 @@
+﻿#region Copyright
+
+// Copyright 2021, TeamDev. All rights reserved.
+// 
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DotNetBrowser.Browser;
+using Mvvm.Wpf.ViewModels;
+
+namespace Mvvm.Wpf.Views
+{
+    /// <summary>
+    ///     Interaction logic for MyBrowserView.xaml
+    /// </summary>
+    public partial class MyBrowserView : UserControl
+    {
+        #region Constructors
+
+        public MyBrowserView()
+        {
+            InitializeComponent();
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AddressBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                AddressBox.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+            }
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = e.NewValue as MyBrowserViewModel;
+
+            if (viewModel != null)
+            {
+                BrowserView.InitializeFrom(viewModel.Browser);
+            }
+        }
+
+        #endregion
+    }
+}
