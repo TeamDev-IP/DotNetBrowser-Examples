10df45b6b882fa09fa6633f2bf57d4d7dea2499c
anna-dolbina <anna.dolbina@gmail.com>
2021-12-13 17:01:05 +0200

    Update ReSharper configuration. Reformat code.

--- vbnet/Popups.Wpf/OpenPopupHandler.vb
+++ vbnet/Popups.Wpf/OpenPopupHandler.vb
@@ -43,8 +43,8 @@ Public Class OpenPopupHandler
         parent = parentElement
     End Sub
 
-    Public Sub Handle(p As OpenPopupParameters) Implements IHandler(
-                                                    Of OpenPopupParameters).Handle
+    Public Sub Handle(p As OpenPopupParameters) _
+        Implements IHandler(Of OpenPopupParameters).Handle
         Dim showPopupAction As Action =
                 Sub()
                     ShowPopup(p.PopupBrowser, p.Rectangle)
