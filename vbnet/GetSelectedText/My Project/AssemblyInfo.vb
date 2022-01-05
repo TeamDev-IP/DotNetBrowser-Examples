5b7feb253de456bdc1f91f8a99c53d5de316a501
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2022-01-05 14:09:18 +0200

    Partly update copyright year in VB.NET  samples

--- vbnet/GetSelectedText/My Project/AssemblyInfo.vb
+++ vbnet/GetSelectedText/My Project/AssemblyInfo.vb
@@ -1,6 +1,6 @@
-#Region "Copyright"
+﻿#Region "Copyright"
 
-' Copyright 2021, TeamDev. All rights reserved.
+' Copyright © 2022, TeamDev. All rights reserved.
 ' 
 ' Redistribution and use in source and/or binary forms, with or without
 ' modification, must retain the above copyright notice and the following
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyConfiguration("")>
 <Assembly: AssemblyCompany("TeamDev")>
 <Assembly: AssemblyProduct("GetSelectedText")>
-<Assembly: AssemblyCopyright("Copyright © 2021, TeamDev. All rights reserved.")>
+<Assembly: AssemblyCopyright("Copyright © 2022, TeamDev. All rights reserved.")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
