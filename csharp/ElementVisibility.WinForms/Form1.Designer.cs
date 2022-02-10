c84cd645f3efb36a4b60f25c84d50de5299c133c
anna-dolbina <anna.dolbina@gmail.com>
2022-02-10 09:16:34 +0200

    Add ElementVisibility.WinForms example for C#

---
+++ csharp/ElementVisibility.WinForms/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ElementVisibility.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.browserView1 = new DotNetBrowser.WinForms.BrowserView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.browserView1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(841, 552);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(835, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change image visibility";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ChangeImageVisibility);
+            // 
+            // browserView1
+            // 
+            this.browserView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserView1.Location = new System.Drawing.Point(3, 3);
+            this.browserView1.Name = "browserView1";
+            this.browserView1.Size = new System.Drawing.Size(835, 516);
+            this.browserView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 552);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private DotNetBrowser.WinForms.BrowserView browserView1;
+    }
+}
+
---
+++ csharp/ElementVisibility.WinForms/Form1.cs
@@ -0,0 +1,80 @@
+﻿#region Copyright
+
+// Copyright © 2022, TeamDev. All rights reserved.
+// 
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+using DotNetBrowser.Browser;
+using DotNetBrowser.Dom;
+using DotNetBrowser.Engine;
+using DotNetBrowser.Js;
+
+namespace ElementVisibility.WinForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly IBrowser browser;
+        private readonly IEngine engine;
+
+        public Form1()
+        {
+            engine = EngineFactory.Create(new EngineOptions.Builder
+            {
+                RenderingMode = RenderingMode.HardwareAccelerated
+            }.Build());
+
+            browser = engine.CreateBrowser();
+            InitializeComponent();
+            browserView1.InitializeFrom(browser);
+
+            browser.Navigation.LoadUrl(Path.GetFullPath("index.html"));
+        }
+
+        private void ChangeImageVisibility(object sender, EventArgs e)
+        {
+            IDocument document = browser.MainFrame.Document;
+
+            // Find element on the loaded web page.
+            IElement img = document.GetElementById("img");
+
+            // Transform it into JsObject.
+            IJsObject imgObject = img as IJsObject;
+
+            // Apply the desired style.
+            IJsObject style = imgObject.Properties["style"] as IJsObject;
+
+            if (style.Properties["display"].ToString() != "none")
+            {
+                style.Properties["display"] = "none";
+            }
+            else
+            {
+                style.Properties["display"] = "inline";
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            engine?.Dispose();
+        }
+    }
+}
