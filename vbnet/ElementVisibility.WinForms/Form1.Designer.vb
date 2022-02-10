65af59c207469cdf38e56ebfab78f82edbd9ea34
anna-dolbina <anna.dolbina@gmail.com>
2022-02-10 09:23:21 +0200

    Add ElementVisibility.WinForms example for VB.NET

---
+++ vbnet/ElementVisibility.WinForms/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿Namespace ElementVisibility.WinForms
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.tableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+			Me.button1 = New System.Windows.Forms.Button()
+			Me.browserView1 = New DotNetBrowser.WinForms.BrowserView()
+			Me.tableLayoutPanel1.SuspendLayout()
+			Me.SuspendLayout()
+			' 
+			' tableLayoutPanel1
+			' 
+			Me.tableLayoutPanel1.ColumnCount = 1
+			Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F))
+			Me.tableLayoutPanel1.Controls.Add(Me.button1, 0, 1)
+			Me.tableLayoutPanel1.Controls.Add(Me.browserView1, 0, 0)
+			Me.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.tableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+			Me.tableLayoutPanel1.Name = "tableLayoutPanel1"
+			Me.tableLayoutPanel1.RowCount = 2
+			Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F))
+			Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F))
+			Me.tableLayoutPanel1.Size = New System.Drawing.Size(841, 552)
+			Me.tableLayoutPanel1.TabIndex = 1
+			' 
+			' button1
+			' 
+			Me.button1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.button1.Location = New System.Drawing.Point(3, 525)
+			Me.button1.Name = "button1"
+			Me.button1.Size = New System.Drawing.Size(835, 24)
+			Me.button1.TabIndex = 0
+			Me.button1.Text = "Change image visibility"
+			Me.button1.UseVisualStyleBackColor = True
+			' 
+			' browserView1
+			' 
+			Me.browserView1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.browserView1.Location = New System.Drawing.Point(3, 3)
+			Me.browserView1.Name = "browserView1"
+			Me.browserView1.Size = New System.Drawing.Size(835, 516)
+			Me.browserView1.TabIndex = 1
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(841, 552)
+			Me.Controls.Add(Me.tableLayoutPanel1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			Me.tableLayoutPanel1.ResumeLayout(False)
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private tableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+		Private WithEvents button1 As System.Windows.Forms.Button
+		Private browserView1 As DotNetBrowser.WinForms.BrowserView
+	End Class
+End Namespace
+
