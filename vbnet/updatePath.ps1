698113772276bae4ebac7e1d9e2cb6ecee391c27
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2022-11-04 17:24:39 +0200

    Update packages path in VB.NET samples

--- vbnet/updatePath.ps1
+++ vbnet/updatePath.ps1
@@ -1,1 +1,1 @@
-(Get-ChildItem -Include "*.vbproj" -Recurse) | foreach {(Get-Content $_) | foreach { $_.Replace("..\packages", "..\..\packages")} | Set-Content $_}
+(Get-ChildItem -Include "*.vbproj" -Recurse) | foreach {(Get-Content $_) | foreach { $_.Replace("..\..\packages", "..\..\..\packages")} | Set-Content $_}
