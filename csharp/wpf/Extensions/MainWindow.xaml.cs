8f819995ebf0d4053a195b53b4b73fcfc9c9c004
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2023-04-03 11:28:42 +0300

    Implement a preview version of Chrome Extensions example

---
+++ csharp/wpf/Extensions/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿#region Copyright
+
+// Copyright © 2023, TeamDev. All rights reserved.
+// 
+// Redistribution and use in source and/or binary forms, with or without
+// modification, must retain the above copyright notice and the following
+// disclaimer.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using System.Windows;
+using DotNetBrowser.Browser;
+using DotNetBrowser.Engine;
+using DotNetBrowser.Extensions.Handlers;
+using DotNetBrowser.Extensions;
+using DotNetBrowser.Handlers;
+using System.ComponentModel;
+
+namespace Extensions.Wpf
+{
+    /// <summary>
+    ///     The sample demonstrates how to install and use 
+    ///     Chrome Extensions in DotNetBrowser.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ExtensionUrl =
+            "https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm/related?hl=en";
+        private IBrowser browser;
+        private IEngine engine;
+        private IExtension extension;
+
+        public MainWindow()
+        {
+            // Create and initialize the IEngine instance.
+            EngineOptions engineOptions = new EngineOptions.Builder
+            {
+                RenderingMode = RenderingMode.HardwareAccelerated
+            }.Build();
+            engine = EngineFactory.Create(engineOptions);
+
+            // Create the IBrowser instance.
+            browser = engine.CreateBrowser();
+
+            InitializeComponent();
+
+            // Initialize the WPF BrowserView control.
+            WebView.InitializeFrom(browser);
+            browser.Navigation.LoadUrl("https://teamdev.com");
+        }
+
+        private void OnInstallExtension(object sender, RoutedEventArgs e)
+        {
+            var extensions = engine.Profiles.Default.Extensions;
+            extensions.ValidatePermissionsHandler =
+                new Handler<ValidatePermissionsParameters, ValidatePermissionsResponse>(
+                    p => ValidatePermissionsResponse.Grant()
+               );
+            extension = extensions.Install(ExtensionUrl).Result;
+            browser.OpenExtensionActionPopupHandler = new PopupHandler(WebView);
+        }
+
+        private void OnLaunchExtension(object sender, RoutedEventArgs e)
+        {
+            if (extension != null)
+            {
+                extension.GetAction(browser).Click();
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            browser.Dispose();
+            engine.Dispose();
+        }
+    }
+}
---
+++ csharp/wpf/Extensions/PopupHandler.cs
@@ -0,0 +1,53 @@
+﻿using DotNetBrowser.Browser.Handlers;
+using DotNetBrowser.Handlers;
+using System.Windows.Threading;
+using System.Windows;
+using System;
+using DotNetBrowser.Wpf;
+using DotNetBrowser.Browser;
+
+namespace Extensions.Wpf
+{
+    internal class PopupHandler : IHandler<OpenExtensionActionPopupParameters, OpenExtensionActionPopupResponse>
+    {
+        private readonly FrameworkElement parent;
+
+        private Dispatcher Dispatcher => parent?.Dispatcher
+                                        ?? Application.Current.Dispatcher;
+
+        public PopupHandler(FrameworkElement parent)
+        {
+            this.parent = parent;
+        }
+
+        public OpenExtensionActionPopupResponse Handle(OpenExtensionActionPopupParameters parameters)
+        {
+            // This handler is invoked before an extension shows its pop-up.
+            // It's the same the pop-up that you see in the browser when
+            // clicking on the extension icon in the browser
+            //
+            // This pop-up is, in fact, a separate browser. You can find it
+            // in the parameters as `parameters.PopupBrowser`. This is a more
+            // or less normal browser: you can use its DOM and automate your
+            // actions.
+            //
+            // This browser has limitations and we will describe them when
+            // the feature is ready.
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                BrowserView browserView = new BrowserView();
+                browserView.InitializeFrom(parameters.PopupBrowser);
+                Window window = new Window { Owner = Window.GetWindow(parent) };
+
+                // These are arbitrary numbers. In the final API version,
+                // DotNetBrowser will create and size the new window automatically.
+                window.Width = 254;
+                window.Height = 480;
+
+                window.Content = browserView;
+                window.Show();
+            }));
+            return OpenExtensionActionPopupResponse.Open();
+        }
+    }
+}
