e42ff1eeb7912bb8707f749a204a19a8d4bb34d3
anna-dolbina <anna.dolbina@gmail.com>
2023-06-22 11:00:48 +0300

    Update Selenium and Unity3D examples

--- csharp/selenium/SeleniumChromeDriver/Form1.Designer.cs
+++ csharp/selenium/SeleniumChromeDriver/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1280, 768);
             this.Text = "Form1";
         }
 
