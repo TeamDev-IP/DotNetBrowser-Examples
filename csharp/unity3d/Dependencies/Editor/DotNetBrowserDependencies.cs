4b4bf36823c832eb366428976c9e5b31482ea15e
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2023-07-31 17:58:07 +0300

    Update Selenium and Unity3D examples

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "2.23.1";
+    private static readonly string Version = "2.23.2";
     static DotNetBrowserDependencies()
     {
         Restore();
