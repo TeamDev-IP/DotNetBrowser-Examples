76f01b870856e9001330a48e6cca6ef5dfd9bf56
Anna Dolbina <1564418+anna-dolbina@users.noreply.github.com>
2023-09-06 11:19:55 +0300

    Update to version 2.23.3 (#149)

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "2.23.2";
+    private static readonly string Version = "2.23.3";
     static DotNetBrowserDependencies()
     {
         Restore();
