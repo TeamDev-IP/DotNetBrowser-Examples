b9debc61616d5b3f712a39487fe7748a2ff42cea
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2024-03-05 19:29:55 +0200

    Add Chromecast WPF VB.NET sample

---
+++ vbnet/wpf/Chromecast/MainWindow.xaml.vb
@@ -0,0 +1,112 @@
+﻿#Region "Copyright"
+
+' Copyright © 2024, TeamDev. All rights reserved.
+' 
+' Redistribution and use in source and/or binary forms, with or without
+' modification, must retain the above copyright notice and the following
+' disclaimer.
+' 
+' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+' "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+' LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+' A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+' OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+' LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+' DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+' THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#End Region
+
+Imports System.Collections.ObjectModel
+Imports DotNetBrowser.Browser
+Imports DotNetBrowser.Cast
+Imports DotNetBrowser.Engine
+
+'''<summary>
+'''    The sample demonstrates how to use Chromecast functionality in DotNetBrowser.
+'''</summary>
+Partial Public Class MainWindow
+    Inherits Window
+
+    Private ReadOnly browser As IBrowser
+    Private ReadOnly engine As IEngine
+    Private castSession As ICastSession
+
+    Public Property Items As New ObservableCollection(Of Receiver)()
+
+    Public Sub New()
+        InitializeComponent()
+
+        Dim engineOptions As EngineOptions =
+                New EngineOptions.Builder With {.MediaRoutingEnabled = True}.Build()
+
+        engine = EngineFactory.Create(engineOptions)
+        browser = engine.CreateBrowser()
+        browserView.InitializeFrom(browser)
+
+        AddHandler browser.Navigation.NavigationFinished, Sub(sender, args)
+            Dispatcher.BeginInvoke(New Action(Sub() navigationBar.Text = browser.Url))
+        End Sub
+
+        browser.Navigation.LoadUrl("https://youtube.com")
+    End Sub
+
+    Private Sub Navigate_Click(sender As Object, e As RoutedEventArgs)
+        If castSession IsNot Nothing Then
+            castSession.Stop()
+            castSession = Nothing
+            receiversBox.SelectedItem = Nothing
+        End If
+
+        browser.Navigation.LoadUrl(navigationBar.Text)
+    End Sub
+
+    Private Sub Receivers_Click(sender As Object, e As RoutedEventArgs)
+        Dispatcher.BeginInvoke(New Action(Sub()
+            receiversBox.ItemsSource = Nothing
+            Items.Clear()
+            browser.Profile.MediaCasting.Receivers.Refresh()
+
+            Dim mediaReceivers As IReadOnlyList(Of IMediaReceiver) =
+                    browser.Profile.MediaCasting.Receivers.AllAvailable
+
+            For Each receiver As IMediaReceiver In mediaReceivers
+                Items.Add(New Receiver With {
+                             .MediaReceiver = receiver,
+                             .Name = receiver.Name
+                             })
+            Next receiver
+
+            receiversBox.ItemsSource = Items
+        End Sub))
+    End Sub
+
+    Private Async Sub ReceiversBox_SelectionChanged(sender As Object,
+                                                    e As SelectionChangedEventArgs)
+        If castSession IsNot Nothing Then
+            Try
+                castSession.Stop()
+            Catch e1 As ObjectDisposedException
+            End Try
+
+            castSession = Nothing
+        End If
+
+        If receiversBox.SelectedItem IsNot Nothing Then
+            Dim selectedReceiver = TryCast(receiversBox.SelectedItem, Receiver)
+            Try
+                castSession = Await browser.Cast.CastContent(selectedReceiver?.MediaReceiver)
+            Catch ex As CastSessionStartFailedException
+                MessageBox.Show(Me, ex.Message, "CastSessionStartFailedException")
+            End Try
+        End If
+    End Sub
+
+    Private Sub Window_Closed(sender As Object, e As EventArgs)
+        engine.Dispose()
+    End Sub
+End Class
+
---
+++ vbnet/wpf/Chromecast/My Project/AssemblyInfo.vb
@@ -0,0 +1,75 @@
+﻿#Region "Copyright"
+
+' Copyright © 2024, TeamDev. All rights reserved.
+' 
+' Redistribution and use in source and/or binary forms, with or without
+' modification, must retain the above copyright notice and the following
+' disclaimer.
+' 
+' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+' "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+' LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+' A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+' OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+' LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+' DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+' THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#End Region
+
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("Chromecast.Wpf")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("TeamDev")>
+<Assembly: AssemblyProduct("Chromecast.Wpf")>
+<Assembly: AssemblyCopyright("Copyright © 2024, TeamDev. All rights reserved.")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+'In order to begin building localizable applications, set
+'<UICulture>CultureYouAreCodingWith</UICulture> in your .vbproj file
+'inside a <PropertyGroup>.  For example, if you are using US english
+'in your source files, set the <UICulture> to "en-US".  Then uncomment the
+'NeutralResourceLanguage attribute below.  Update the "en-US" in the line
+'below to match the UICulture setting in the project file.
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+
+'The ThemeInfo attribute describes where any theme specific and generic resource dictionaries can be found.
+'1st parameter: where theme specific resource dictionaries are located
+'(used if a resource is not found in the page,
+' or application resource dictionaries)
+
+'2nd parameter: where the generic resource dictionary is located
+'(used if a resource is not found in the page,
+'app, and any theme specific resource dictionaries)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2ce651da-ffaa-4129-9b88-519399abbfa4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
