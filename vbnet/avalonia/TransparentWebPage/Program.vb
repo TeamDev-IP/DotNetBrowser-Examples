53b6ece0e24f806d4cae869661ddb630b6f3e940
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2024-04-03 17:25:12 +0200

    Add Avalonia transparency samples (#165)
    * Add Avalonia transparency C# sample
    
    * Add Avalonia transparency VB.NET sample
    
    * Add description
    
    * Update DotNetBrowser version

---
+++ vbnet/avalonia/TransparentWebPage/Program.vb
@@ -0,0 +1,39 @@
+﻿#Region "Copyright"
+
+' Copyright © 2024, TeamDev. All rights reserved.
+' 
+' Redistribution and use in source and/or binary forms, with or without
+' modification, must retain the above copyright notice and the following
+' disclaimer.
+' 
+' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+' "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+' LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+' A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+' OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+' LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+' DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+' THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+' OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#End Region
+
+Imports System
+Imports Avalonia
+
+Friend Class Program
+    ' Initialization code. Don't use any Avalonia, third-party APIs or any
+    ' SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    ' yet and stuff might break.
+    <STAThread>
+    Public Shared Sub Main(ByVal args() As String)
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args)
+    End Sub
+
+    ' Avalonia configuration, don't remove; also used by visual designer.
+    Public Shared Function BuildAvaloniaApp() As AppBuilder
+        Return AppBuilder.Configure(Of App)().UsePlatformDetect().LogToTrace()
+    End Function
+End Class
