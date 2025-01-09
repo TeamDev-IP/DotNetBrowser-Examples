95b3561fac033a88bc59d425cbb14eb2d8f7082e
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2025-01-09 11:23:02 +0100

    Update copyrights year in V3 (#181)

--- vbnet/winforms/FindText/My Project/AssemblyInfo.vb
+++ vbnet/winforms/FindText/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("FindText.WinForms")>
-<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyCopyright("Copyright © 2025")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
