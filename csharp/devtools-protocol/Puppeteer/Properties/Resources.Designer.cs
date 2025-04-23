67d8cad214ca72330db525474d4adec88eda1c41
Eugene Komar <evhenii.komar@teamdev.com>
2025-04-23 12:30:47 +0300

    Implement Playwright and Puppeteer examples (#192)
    This changeset implements examples that show how to interact with DotNetBrowser DevTools using Playwright and Puppeteer APIs.

--- csharp/devtools-protocol/Puppeteer/Properties/Resources.Designer.cs
+++ csharp/devtools-protocol/Puppeteer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SeleniumChromeDriver.Properties {
+namespace PuppeteerExample.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SeleniumChromeDriver.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SeleniumChromeDriver.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PuppeteerExample.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
