9863f3a1dee0be6de9879ff537186abb6ca84bae
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2025-06-20 12:32:22 +0200

    Update to version 2.27.14 (#198)
    * Update C# and VB.NET samples
    
    * Update Selenium samples
    
    * Update version in Unity3d sample
    
    ---------
    
    Co-authored-by: TeamcityDeployAgent <TeamcityDeployAgent@users.noreply.github.com>

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "2.27.13";
+    private static readonly string Version = "2.27.14";
     static DotNetBrowserDependencies()
     {
         Restore();
