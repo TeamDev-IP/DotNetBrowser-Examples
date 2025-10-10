7a1a5d22ed6673c4184565c776965920ef9acfaa
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2025-10-10 16:15:08 +0200

    Update to version 3.3.4 (#214)
    * Update C# and VB.NET samples
    
    * Update Unity sample
    
    * Update Selenium sample
    
    ---------
    
    Co-authored-by: TeamcityDeployAgent <TeamcityDeployAgent@users.noreply.github.com>

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "3.3.3";
+    private static readonly string Version = "3.3.4";
     static DotNetBrowserDependencies()
     {
         Restore();
