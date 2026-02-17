18f1645cbbd8f8521dc5af4ca7a2b52422132b61
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2026-02-17 17:48:11 +0200

    Update Unity3d sample

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "3.4.0";
+    private static readonly string Version = "3.5.0";
     static DotNetBrowserDependencies()
     {
         Restore();
