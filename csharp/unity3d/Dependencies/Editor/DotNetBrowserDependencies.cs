0a9c621d3cf4ffa5856e07416d42d15e0b74c178
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2026-07-21 14:29:56 +0200

    Update to version 4.2.1 (#256)
    * Update C# and VB.NET samples
    
    * Update Unity3d
    
    ---------
    
    Co-authored-by: TeamcityDeployAgent <TeamcityDeployAgent@users.noreply.github.com>

--- csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
+++ csharp/unity3d/Dependencies/Editor/DotNetBrowserDependencies.cs
@@ -29,7 +29,7 @@ using UnityEditor;
 public class DotNetBrowserDependencies
 {
     private static readonly string AssembliesPath = Path.Combine(Application.dataPath, "Assemblies");
-    private static readonly string Version = "4.2.0";
+    private static readonly string Version = "4.2.1";
     static DotNetBrowserDependencies()
     {
         Restore();
